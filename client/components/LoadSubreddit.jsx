21f94666a13337abedc56c87618f00dffbc683d2
mattritani <mattritani@gmail.com>
2018-09-12 11:11:37 +1200

    one thing working - snack time

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -3,9 +3,17 @@ import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 
 const LoadSubreddit = ({dispatch}) => (
-  <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-    Fetch Posts
-  </button>
-)
+  <React.Fragment>
+    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+        Get NZ Subreddit!
+      </button>
 
+      <form onSubmit={function(e){
+        e.preventDefault()
+      }}><input id='newSub' type='text'></input><button onClick={() =>
+        dispatch(fetchPosts(document.getElementById('newSub').value))
+      }> Get new subreddit! </button></form>
+
+    </React.Fragment>
+)
 export default connect()(LoadSubreddit)
